a474d315cc910bebd658c5609c93827cf6b2cf8b
AhmedKhalil777 <progeng_ahmed_khalil@outlook.com>
2021-11-04 09:30:09 +0200

    Add WIP

---
+++ InstallRedisMSI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InstallRedisMSI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===================== Installing a Service in a quiet mode ==================================");
+            Console.WriteLine("===================== Trying To Install Redis MSI          ==================================");
+            var strCmdText = "/C msiexec /quiet /i Redis-x64-3.0.504.msi";
+            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            Console.WriteLine("===================== Installation Completed Successfully  ===============================");
+            Console.ReadKey();
+        }
+    }
+}
